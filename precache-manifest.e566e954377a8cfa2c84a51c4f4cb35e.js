a8a387152577abc6fdb06a6a25ef636d00c29e3d
Monika Fac <monika.fac@gmail.com>
2019-12-15 06:06:11 +0100

    Updates

---
+++ precache-manifest.e566e954377a8cfa2c84a51c4f4cb35e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8c25b3137dd96e8fa4d5c97644eb00",
+    "url": "/bigFatFish_restaurant/index.html"
+  },
+  {
+    "revision": "e9742654a219b4594f18",
+    "url": "/bigFatFish_restaurant/static/css/main.d3bbca23.chunk.css"
+  },
+  {
+    "revision": "8349965e0fc25973e9e1",
+    "url": "/bigFatFish_restaurant/static/js/2.8a3648aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bigFatFish_restaurant/static/js/2.8a3648aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9742654a219b4594f18",
+    "url": "/bigFatFish_restaurant/static/js/main.25b72e3d.chunk.js"
+  },
+  {
+    "revision": "63d5c196adbb3f2acbd1",
+    "url": "/bigFatFish_restaurant/static/js/runtime-main.1e9e899c.js"
+  },
+  {
+    "revision": "688edae9a8d29349ca6821e1d3498625",
+    "url": "/bigFatFish_restaurant/static/media/cafe-3537801_1920.688edae9.jpg"
+  },
+  {
+    "revision": "8b20604ba891cc1a0614747142516d8b",
+    "url": "/bigFatFish_restaurant/static/media/platter-2009590_1920.8b20604b.jpg"
+  },
+  {
+    "revision": "263fa55fe03709549ae8eef4ebc82465",
+    "url": "/bigFatFish_restaurant/static/media/wine-1761613_1920.263fa55f.jpg"
+  }
+]);
